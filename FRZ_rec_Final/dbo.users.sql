671dadec239c0f25e1daf1dc3096be2303452b50
amarhbaneen <amaralzana@gmail.com>
2020-01-06 16:25:55 +0200

    delteing files

--- FRZ_rec_Final/dbo.users.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[users] (
-    [Id]       INT           NOT NULL IDENTITY,
-    [username] NVARCHAR (50) NOT NULL,
-    [password] NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
